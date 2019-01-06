a56f07295bfe2c2df0181121a4e8f33126088e9a
Benjamin Assadsolimani <benjamin.assadsolimani@gmail.com>
2019-01-06 14:09:39 +0100

    Document pm2 RPC endpoint class

--- src/endpoints/pm2.ts
+++ src/endpoints/pm2.ts
@@ -1,28 +1,23 @@
-/**
- * PM2 RPC middlewares
- */
-
 import * as pm2Sync from 'pm2';
 import * as bluebird from 'bluebird';
 import { RpcEndpoint } from '../rpc-endpoint';
 
+// Promisify pm2 to eliminate callbacks
 const pm2 = bluebird.promisifyAll(pm2Sync);
 
+/** Pm2 RPC endpoint class */
 export class Pm2Endpoint extends RpcEndpoint {
   constructor(log, rpcHelper) {
     super('/pm2', pm2, log, rpcHelper);
   }
 
+  /** Execute pm2 RPC call, use the async pendant of the requested method and remove pm2's array wrapping around returned result and error objects  */
   async execute(method, params) {
     try {
-      const methodAsync = `${method}Async`;
-      const result = await pm2[methodAsync](...params);
-      return result;
+      const result = await super.execute(`${method}Async`, params);
+      return Array.isArray(result) ? result[0] : result;
     } catch (err) {
-      if (Array.isArray(err)) {
-        throw err[0];
-      }
-      throw err;
+      throw Array.isArray(err) ? err[0] : err;
     }
   }
 }
