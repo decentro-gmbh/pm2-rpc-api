966d50df3ef17b1d7dbf4d3e9257482c5421847e
Benjamin Assadsolimani <benjamin.assadsolimani@gmail.com>
2019-01-07 13:35:31 +0100

    Remove execa and pm2 from examples and use built-ins instead (console, Math)

--- example/server.ts
+++ example/server.ts
@@ -1,20 +1,18 @@
-/**
- * Example server
- */
-
-import * as execa from 'execa';
-import * as child_process from 'child_process';
 import { RpcServer } from '../src/rpc-server';
-import { Pm2Endpoint } from './pm2';
 
+/** Example RPC server */
 const server = new RpcServer({
   host: '0.0.0.0',
-  authentication: true,
   envPrefix: 'SERVER_',
+  authentication: true,
   apikeyhash: '959c9f50aef1bc129a0e16564319a1b36515d570513079b6c73c72a5709abdce', // sha256('secr3t')
 });
 
-server.addEndpoint('/child_process', child_process);
-server.addEndpoint('/execa', execa);
-server.addEndpoint(new Pm2Endpoint());
+server.addEndpoint('/console', console);
+server.addEndpoint('/math', Math);
+server.addEndpoint('/custom', {
+  pick(...items) {
+    return `I like this one best: ${items[Math.floor(Math.random() * items.length)]}`;
+  },
+});
 server.start();
