0bbc701c368ba0e9ee0258811a6aa19292db68f4
Benjamin Assadsolimani <benjamin.assadsolimani@gmail.com>
2019-01-07 14:20:30 +0100

    Document the RpcServer class and add async start() and stop() methods

--- src/rpc-server.ts
+++ src/rpc-server.ts
@@ -1,8 +1,8 @@
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
-import { Express } from 'express';
 import * as nconf from 'nconf';
-import { ILogger, IServerOptions } from './interfaces';
+import { Server } from 'http';
+import { ILogger, IServerOptions, IEndpointOptions } from './interfaces';
 import { generateAuthMiddleware } from './authentication';
 import { RpcEndpoint } from './rpc-endpoint';
 export { RpcEndpoint } from './rpc-endpoint';
@@ -20,6 +20,7 @@ export class RpcServer {
   private apikeyhash: null | string;
 
   private endpoints: Array<RpcEndpoint> = [];
+  private httpServer: Server = null;
 
   constructor(options: IServerOptions = {}) {
     this.log = options.logger || {
@@ -90,34 +91,51 @@ export class RpcServer {
   protected registerRpcEndpoints(server) {
     this.endpoints.forEach((rpcEndpoint) => {
       this.log.info(`Registering new endpoint: ${rpcEndpoint.getPath()}`);
+      rpcEndpoint.setLogger(this.log);
       rpcEndpoint.register(server);
     });
   }
 
-  public addEndpoint(path: string, module?: any);
+  /**
+   * Add an RPC endpoint
+   * @param path Path of the endpoint (e.g., '/execute')
+   * @param module Module whose methods can be executed via the RPC endpoint
+   * @param options RPC endpoint options
+   */
+  public addEndpoint(path: string, module: any, options?: IEndpointOptions);
+  /**
+   *  Add a custom RPC endpoint
+   * @param rpcEndpoint Instance of a class extending the 'RpcEndpoint' class
+   */
   public addEndpoint(rpcEndpoint: RpcEndpoint): void;
-  public addEndpoint(param1: string | RpcEndpoint, param2?: any) {
+  public addEndpoint(param1: string | RpcEndpoint, param2?: any, param3?: IEndpointOptions) {
     if (typeof param1 === 'string' && param2) {
       const path: string = param1;
       const module = param2;
-      const rpcEndpoint = new RpcEndpoint(path, module);
+      const options = param3;
+      const rpcEndpoint = new RpcEndpoint(path, module, options);
       this.endpoints.push(rpcEndpoint);
+
     } else if (typeof param1 === 'object') {
       const rpcEndpoint: RpcEndpoint = param1;
       this.endpoints.push(rpcEndpoint);
     }
   }
 
-  /** Start the HTTP JSON-RPC API */
-  public start(): Express {
+  /**
+   * Start the RPC server:
+   *   - Register middlewares for each added RPC endpoint
+   *   - Listen on specified host and port
+   */
+  public async start(): Promise<Server> {
     if (this.disabled) {
       this.log.err('Server is disabled, exiting.');
       return null;
     }
 
-    const server = express();
-    server.use(bodyParser.json());
-    server.disable('x-powered-by');
+    const app = express();
+    app.use(bodyParser.json());
+    app.disable('x-powered-by');
 
     // Register authentication middleware
     if (!this.authentication && this.apikeyhash) {
@@ -126,16 +144,16 @@ export class RpcServer {
       this.log.err('Authentication is enabled but no API key hash is given, exiting.');
       process.exit(1);
     } else if (this.authentication && this.apikeyhash) {
-      server.use('*', generateAuthMiddleware(this.apikeyhash, this.log));
+      app.use('*', generateAuthMiddleware(this.apikeyhash, this.log));
     } else {
       this.log.info('Authentication disabled');
     }
 
     // Register RPC endpoints
-    this.registerRpcEndpoints(server);
+    this.registerRpcEndpoints(app);
 
-    // Error middleware
-    server.use(function errorMiddleware(err, req, res, next) {
+    // Register error middleware
+    app.use(function errorMiddleware(err, req, res, next) {
       if (err.type === 'entity.parse.failed') {
         return res.status(400).json({
           code: -32700,
@@ -143,17 +161,37 @@ export class RpcServer {
           data: err.stack,
         });
       }
-      return res.status(500).json({
 
+      return res.status(500).json({
+        code: -32000,
+        message: 'An unexpected server error has occurred',
+        data: err.stack,
       });
     });
 
     // Start listening
-    server.listen(this.port, this.host, () => {
-      this.log.info(`Server listening on ${this.host}:${this.port}`);
+    await new Promise((resolve, reject) => {
+      this.httpServer = app.listen(this.port, this.host, () => {
+        this.log.info(`Server listening on ${this.host}:${this.port}`);
+        resolve();
+      });
     });
 
-    return server;
+    return this.httpServer;
+  }
+
+  /** Stop the RPC server (print an error message if the server is not running) */
+  public async stop(): Promise<void> {
+    await new Promise((resolve, reject) => {
+      if (this.httpServer) {
+        this.httpServer.close(() => {
+          this.log.info('Server stopped.');
+          resolve();
+        });
+      } else {
+        this.log.err('Cannot stop server: server has not been started!');
+      }
+    });
   }
 
 }
